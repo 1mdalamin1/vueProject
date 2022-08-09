d392e049e611d09d2c47c77cbf9cea0924366596
MD AL AMIN <01mdalamin1@gmail.com>
2022-08-09 11:34:30 +0600

    add node modules

---
+++ vueProject.php
@@ -0,0 +1,28 @@
+
+<!-- 
+    
+npm install -g @vue/cli |>> https://cli.vuejs.org/guide/installation.html
+npm audit fix --force
+vue --version
+
+npm update -g @vue/cli
+
+vue create project_dashboard |-> go to create project
+cd project_dashboard
+npm install vuex@next --save => if needed
+npm install vue-router@4 => if needed
+npm install => if needed
+npm run serve => -->
+
+
+
+
+
+
+
+
+
+
+
+
+
